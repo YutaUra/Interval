75850bd3bc35dd0776d2731564745e88b9b70083
masaki-tandf <52307659+masaki-tandf@users.noreply.github.com>
2019-07-26 13:15:43 +0900

    Update test.py
    aをbに、ｄをaに書き換えたほうが分かりやすいと思います。

--- interval/test.py
+++ interval/test.py
@@ -1,7 +1,7 @@
 from .Interval import *
 
-d = Intervals(([1, 3], [4, 6]))
-d.marge()
-print(d)
-a = Intervals(([2, 4], [4, 5]))
-print(a in d)
+a = Intervals(([1, 3], [4, 6]))
+a.marge()
+print(a)
+b = Intervals(([2, 4], [4, 5]))
+print(b in a)
